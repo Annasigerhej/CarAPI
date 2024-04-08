f9b4a954906a0a3a19514b3a6bd92e760f2f1b14
Anna Christine Vinkel <annachristinevinkel@gmail.com>
2024-04-08 16:18:38 +0200

    Added controller

---
+++ CarAPI/Controllers/CarTestController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarAPI.Data;
+using CarAPI.Models;
+
+namespace CarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarTestController : ControllerBase
+    {
+        private readonly CarDBContext _context;
+
+        public CarTestController(CarDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarTest
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+            return await _context.Cars.ToListAsync();
+        }
+
+        // GET: api/CarTest/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCar(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return car;
+        }
+
+        // PUT: api/CarTest/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCar(int id, Car car)
+        {
+            if (id != car.LicensePlate)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(car).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CarTest
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Car>> PostCar(Car car)
+        {
+            _context.Cars.Add(car);
+            await _context.SaveChangesAsync();
+
+            // return CreatedAtAction("GetCar", new { id = car.LicensePlate }, car);
+              return CreatedAtAction(nameof(GetCar), new { id = car.LicensePlate }, car);
+        }
+
+        // DELETE: api/CarTest/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cars.Remove(car);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarExists(int id)
+        {
+            return _context.Cars.Any(e => e.LicensePlate == id);
+        }
+    }
+}
